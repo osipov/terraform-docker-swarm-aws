001f9ce2c16d5bac6ea01e4f25dd0c6a2b41203f
Carl Osipov <osipov@users.noreply.github.com>
2021-01-13 16:39:21 -0500

    Update eips.tf

--- eips.tf
+++ eips.tf
@@ -3,6 +3,10 @@ resource "aws_eip" "managers" {
   instance = module.docker.manager_instance_ids[count.index]
   vpc      = true
 }
+  
+output "manager_ip_address" {
+  value = aws_eip.managers[0].public_ip
+}
 
 output "manager_ip_addresses" {
   value = aws_eip.managers.*.public_ip
