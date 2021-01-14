1353082c13d521116f9eae877b4ff115e6b36cb3
Carl Osipov <osipov@users.noreply.github.com>
2021-01-14 11:03:06 -0500

    Update docker.tf

--- docker.tf
+++ docker.tf
@@ -14,6 +14,31 @@ variable "name" {
   type = string
 }
 
+variable "instances" {
+  type = int
+  default = 1
+}
+
+variable "worker_instance_type" {
+  type = string
+  default = "t3.micro"
+}
+
+variable "manager_instances" {
+  type = int
+  default = 1
+}
+
+variable "manager_instance_type" {
+  type = string
+  default = "t3.micro"
+}
+
+variable "volume_size" {
+  type = int
+  default = 8
+}
+
 module "docker" {
   source = "./src"
 
@@ -27,13 +52,13 @@ module "docker" {
   store_join_tokens_as_tags   = true
   cloudwatch_logs             = false
 
-  managers = 1
-  instance_type_manager = "t3.micro"
+  managers = var.manager_instances
+  instance_type_manager = var.manager_instance_type 
 
-  workers = 1
-  instance_type_worker = "t3.micro"
+  workers = var.instances
+  instance_type_worker = var.worker_instance_type #"t3.micro"
 
-#   volume_size = "32"
+  volume_size = var.volume_size
 
   additional_security_group_ids = [
     aws_security_group.exposed.id,
